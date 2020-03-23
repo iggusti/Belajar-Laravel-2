986815dba2b06d5c7b4af3328e93cb6c8cbd6078
Ilham Gumantung Gusti <iggusti@gmail.com>
2020-03-23 13:45:13 +0700

    Add Login annd Registration without Edit and Delete

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Student;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::all();
+
+        // dump($students);
+
+        $students = $students->sortBy('nama');
+        
+
+        // return view('students.index', ['students' => $students]);
+       
+        return view('students.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validation
+        $request->validate([
+            'nama' => 'required',
+            'nim' => 'required',
+            'email' => 'required|email|unique:students',
+            'jurusan' => 'required',
+            ]);
+
+        // cara 1
+        // $student = new Student;
+        // $student->nama = $request->nama;
+        // $student->nim = $request->nim;
+        // $student->email = $request->email;
+        // $student->jurusan = $request->jurusan;
+
+        // $student->save();
+        
+        // cara 2
+        // Student::create([
+        //     'nama' => $request->nama,
+        //     'nim' => $request->nim,
+        //     'email' => $request->email,
+        //     'jurusan' => $request->jurusan
+        // ]);
+        
+        // cara 3
+        // Student::create($request->all());
+
+        Student::create([
+            'nama' => $request['nama'],
+            'nim' => $request['nim'],
+            'email' => $request['email'],
+            'jurusan' => $request['jurusan']
+            ]);
+
+        return redirect('/students')->with('status', 'Data Mahasiswa Berhasil Ditambahkan!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        return view('students.show', compact('student'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
